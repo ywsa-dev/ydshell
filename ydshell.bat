a2a60ed40246da2b8fbd27abd0eed32e41278d18
ywsa-dev <ywsadev@gmail.com>
2026-07-25 18:45:43 +0900

    도움말 추가

--- ydshell.bat
+++ ydshell.bat
@@ -1,6 +1,6 @@
 @echo off
 title ydshell
-set ver=ydshell version:[ywsa-dev:ydshell:1.9]
+set ver=ydshell version:[ywsa-dev:ydshell:2.0]
 echo ywsa-dev cmd command launcher
 echo You can view the help information using the "help" command.
 echo %ver%
@@ -13,6 +13,8 @@ set "first=%%a"
 set "second=%%b"
 )
 if "%first%"=="help" (
+if "%second%" == "/?" (echo Displays help for each command.&goto LOOP)
+echo help  Displays help for each command.
 echo veris  You can see computer os version.
 echo ver  You can see ydshell version.
 echo now  You can see time or date.
@@ -29,10 +31,14 @@ echo ydshell  Ydshell in ydshell.
 echo               /shutdown exit ydshell.
 echo waitkey  pause and nul.
 echo               /real only pause.
+echo start  start program.
+echo help:/?
 ) else if "%first%"=="veris" (
+if "%second%" == "/?" (echo You can see computer os version.&goto LOOP)
 ver
+echo.
 ) else if "%first%"=="now" (
-if "%second%"=="" (
+if "%second%" == "/?" (echo You can see time or date. &echo /date only see date.&echo /time only see time.&goto LOOP) else if "%second%"=="" (
 echo date: %date%
 echo time: %time%
 ) else if "%second%"=="/date" (
@@ -43,17 +49,18 @@ echo %time%
 echo The command is invalid.
 )
 ) else if "%first%"=="clear" (
+if "%second%" == "/?" (echo You can organize the screen.&goto LOOP)
 cls
 ) else if "%first%"=="exit" (
-if %noexit% == 0 (
+if "%second%" == "/?" (echo You can exit Ydshell.&goto LOOP) else if %noexit% == 0 (
 echo shutdown ydshell...
 timeout /t 1 /nobreak >nul
 exit
 ) else (
-set /a noexit=%noexit-1
+set /a noexit-=1
 )
 ) else if "%first%"=="cmdstart" (
-if "%second%"=="" (
+if "%second%" == "/?" (echo start cmd.&echo /changepowershell not start cmd. start powershell.&goto LOOP) else if "%second%"=="" (
 cmd
 ) else if "%second%" == "/changepowershell" (
 powershell
@@ -61,17 +68,21 @@ powershell
 echo The command is invalid.
 )
 ) else if "%first%" == "rem" (
+if "%second%" == "/?" (echo It's same:cmd rem.&goto LOOP)
 rem rem
 ) else if "%first%" == "tsli" (
+if "%second%" == "/?" (echo You can know what program running.&goto LOOP)
 tasklist /fi "status eq running"
 ) else if "%first%" == "destory" (
+if "%second%" == "/?" (echo You can definitely delete the file.&goto LOOP)
 takeown /f "%second%"
 icacls "%second%" /grant Administrators:F
 del "%second%"
 ) else if "%first%" == "ver" (
+if "%second%" == "/?" (echo You can see ydshell version.&goto LOOP)
 echo %ver%
 ) else if "%first%" == "ydshell" (
-if "%second%" == "" (
+if "%second%" == "/?" (echo ydshell  Ydshell in ydshell.&echo /shutdown exit ydshell.&goto LOOP) else if "%second%" == "" (
 echo.
 echo ywsa-dev cmd command launcher
 echo You can view the help information using the "help" command.
@@ -90,7 +101,11 @@ set /a noexit=%noexit-1
 echo The command is invalid.
 )
 ) else if "%first%" == "waitkey" (
+if "%second%" == "/?" (echo waitkey  pause and nul.&echo /real only pause.&goto LOOP)
 if "%second%" == "" (pause >nul) else if "%second%" == "/real" (pause) else (echo The command is invalid.)
+) else if "%first%" == "start" (
+if "%second%" == "/?" (echo start program.&goto LOOP)
+start %second%
 ) else (
 echo "%first%" is an unknown command.
 )
